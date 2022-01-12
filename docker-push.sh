1d6a30163c9215dfa276ad9d2ca4dbad829f0678
no <no>
2022-01-12 21:33:38 +0000

    fix: docker push

--- docker-push.sh
+++ docker-push.sh
@@ -4,4 +4,4 @@ NAME=$(jq -r .name < package.json)
 VERSION=$(jq -r .version < package.json)
 
 docker build -t cornon/$NAME:$VERSION -t cornon/$NAME:latest . \
-    && docker push cornon/$NAME
+    && docker push --all-tags cornon/$NAME
